4e77a25cda38e5a69e179da92ba87380c539df1e
bouramadevweb <bouramadevweb@gmail.com>
2025-06-23 11:55:00 +0200

    modification de l'essemble du model transport

--- transport/models.py
+++ transport/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-
 # =======================
 # ENUMS – Constantes pour les choix
 # =======================
@@ -28,7 +24,7 @@ ETAT_PAIEMENT_CHOICES = [
     ('bon', 'Bon'),
     ('moyen', 'Moyen'),
     ('mauvais', 'Mauvais'),
-    ('bloque', 'Bloqué'),
+    ('bloqué', 'Bloqué'),
 ]
 
 TYPE_CLIENT_CHOICES = [
@@ -37,14 +33,14 @@ TYPE_CLIENT_CHOICES = [
 ]
 
 STATUT_CAUTION_CHOICES = [
-    ('bloquee', 'Bloquée'),
-    ('debloquee', 'Débloquée'),
+    ('bloquée', 'Bloquée'),
+    ('débloquée', 'Débloquée'),
 ]
 
 STATUT_MISSION_CHOICES = [
-    ('en_cours', 'En cours'),
-    ('terminee', 'Terminée'),
-    ('annulee', 'Annulée'),
+    ('en cours', 'En cours'),
+    ('terminée', 'Terminée'),
+    ('annulée', 'Annulée'),
 ]
 
 # =======================
@@ -60,9 +56,6 @@ class Entreprise(models.Model):
     date_creation = models.DateField(auto_now_add=True)
     statut = models.CharField(max_length=10, choices=STATUT_ENTREPRISE_CHOICES, default='active')
 
-    def __str__(self):
-        return self.pk_entreprise,  self.nom, self.secteur_activite, self.email_contact, self.telephone_contact, self.date_creation, self.statut
-
 
 class Utilisateur(models.Model):
     pk_utilisateur = models.CharField(max_length=250, primary_key=True)
@@ -70,13 +63,9 @@ class Utilisateur(models.Model):
     nom_utilisateur = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     mot_de_passe_hash = models.CharField(max_length=255)
-    role = models.CharField(max_length=15, choices=ROLE_UTILISATEUR_CHOICES, default='utilisateur')
+    role = models.CharField(max_length=10, choices=ROLE_UTILISATEUR_CHOICES, default='utilisateur')
     actif = models.BooleanField(default=True)
     date_creation = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.pk_utilisateur}, {self.entreprise}, {self.nom_utilisateur}, {self.email}, {self.email}, {self.mot_de_passe_hash}, {self.role}, {self.actif}, {self.date_creation}"
-
 
 class Chauffeur(models.Model):
     pk_chauffeur = models.CharField(max_length=250, primary_key=True)
@@ -85,9 +74,6 @@ class Chauffeur(models.Model):
     prenom = models.CharField(max_length=50)
     telephone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.pk_chauffeur}, {self.entreprise},{self.nom}, {self.prenom}, {self.telephone}, {self.email}"
 
 
 class UtilisateurChauffeur(models.Model):
@@ -98,9 +84,6 @@ class UtilisateurChauffeur(models.Model):
     actif = models.BooleanField(default=True)
     date_creation = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.pk_utilisateur_chauffeur},{self.chauffeur}, {self.email}, {self.mot_de_passe_hash}, {self.actif} ,{self.date_creation}"
-
 
 class Camion(models.Model):
     pk_camion = models.CharField(max_length=250, primary_key=True)
@@ -108,18 +91,12 @@ class Camion(models.Model):
     modele = models.CharField(max_length=50, blank=True, null=True)
     capacite_tonnes = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
-    def __str__(self):
-        return f"{self.pk_camion},{self.immatriculation},{self.modele}, {self.capacite_tonnes}"
-
 
 class Mecanicien(models.Model):
     pk_mecanicien = models.CharField(max_length=250, primary_key=True)
     nom = models.CharField(max_length=100)
     telephone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.pk_mecanicien}, {self.nom}, {self.telephone}, {self.email}"
 
 
 class Fournisseur(models.Model):
@@ -131,9 +108,6 @@ class Fournisseur(models.Model):
     fiabilite = models.CharField(max_length=10, choices=FIABILITE_CHOICES, default='bon')
     commentaire = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return  f"{self.pk_fournisseur}, {self.telephone}, {self.email}, {self.adresse}, {self.fiabilite}, {self.commentaire},  {self.nom}"
-
 
 class Transitaire(models.Model):
     pk_transitaire = models.CharField(max_length=250, primary_key=True)
@@ -144,22 +118,16 @@ class Transitaire(models.Model):
     etat_paiement = models.CharField(max_length=10, choices=ETAT_PAIEMENT_CHOICES, default='bon')
     commentaire = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.nom
-
 
 class Client(models.Model):
     pk_client = models.CharField(max_length=250, primary_key=True)
     nom = models.CharField(max_length=100)
-    type_client = models.CharField(max_length=15, choices=TYPE_CLIENT_CHOICES)
+    type_client = models.CharField(max_length=10, choices=TYPE_CLIENT_CHOICES)
     telephone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     score_fidelite = models.IntegerField(default=100)
     etat_paiement = models.CharField(max_length=10, choices=ETAT_PAIEMENT_CHOICES, default='bon')
     commentaire = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nom
 
 
 class Conteneur(models.Model):
@@ -169,9 +137,6 @@ class Conteneur(models.Model):
     poids = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     transitaire = models.ForeignKey(Transitaire, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.numero_conteneur
 
 
 class ContratTransport(models.Model):
@@ -183,14 +148,11 @@ class ContratTransport(models.Model):
     date_debut = models.DateField()
     date_limite_retour = models.DateField()
     caution = models.DecimalField(max_digits=10, decimal_places=2)
-    statut_caution = models.CharField(max_length=15, choices=STATUT_CAUTION_CHOICES, default='bloquee')
+    statut_caution = models.CharField(max_length=10, choices=STATUT_CAUTION_CHOICES, default='bloquée')
     commentaire = models.TextField(blank=True, null=True)
     signature_chauffeur = models.BooleanField(default=False)
     signature_client = models.BooleanField(default=False)
     signature_transitaire = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.pk_contrat} ,{self.conteneur}, {self.client}, {self.transitaire}"
 
 
 class Affectation(models.Model):
@@ -199,9 +161,6 @@ class Affectation(models.Model):
     camion = models.ForeignKey(Camion, on_delete=models.CASCADE)
     date_affectation = models.DateField(auto_now_add=True)
     date_fin_affectation = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.chauffeur} - {self.camion}"
 
 
 class FraisTrajet(models.Model):
@@ -213,9 +172,6 @@ class FraisTrajet(models.Model):
 
     class Meta:
         unique_together = ('origine', 'destination')
-
-    def __str__(self):
-        return f"{self.origine} - {self.destination}"
 
 
 class Mission(models.Model):
@@ -229,7 +185,56 @@ class Mission(models.Model):
     prix_unitaire_par_tonne = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     frais_trajet = models.ForeignKey(FraisTrajet, on_delete=models.SET_NULL, blank=True, null=True)
     contrat = models.ForeignKey(ContratTransport, on_delete=models.CASCADE)
-    statut = models.CharField(max_length=15, choices=STATUT_MISSION_CHOICES, default='en_cours')
+    statut = models.CharField(max_length=10, choices=STATUT_MISSION_CHOICES, default='en cours')
+
+
+class MissionConteneur(models.Model):
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    conteneur = models.ForeignKey(Conteneur, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('mission', 'conteneur')
+
+
+class Reparation(models.Model):
+    pk_reparation = models.CharField(max_length=250, primary_key=True)
+    camion = models.ForeignKey(Camion, on_delete=models.CASCADE)
+    chauffeur = models.ForeignKey(Chauffeur, on_delete=models.SET_NULL, blank=True, null=True)
+    date_reparation = models.DateField()
+    cout = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+
+class ReparationMecanicien(models.Model):
+    reparation = models.ForeignKey(Reparation, on_delete=models.CASCADE)
+    mecanicien = models.ForeignKey(Mecanicien, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('reparation', 'mecanicien')
+
+
+class PieceReparee(models.Model):
+    pk_piece = models.CharField(max_length=250, primary_key=True)
+    reparation = models.ForeignKey(Reparation, on_delete=models.CASCADE)
+    nom_piece = models.CharField(max_length=100)
+    quantite = models.PositiveIntegerField(default=1)
+    cout_unitaire = models.DecimalField(max_digits=10, decimal_places=2)
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+
+
+class PaiementMission(models.Model):
+    pk_paiement = models.CharField(max_length=250, primary_key=True)
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE)
+    montant_total = models.DecimalField(max_digits=10, decimal_places=2)
+    montant_avance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    montant_solde = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    commission_transitaire = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    caution_retiree = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    caution_remboursee = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date_paiement = models.DateField(auto_now_add=True)
+    mode_paiement = models.CharField(max_length=50, blank=True, null=True)
+    observation = models.TextField(blank=True, null=True)
 
     def __str__(self):
-        return self.pk_mission
+        return f"{self.pk_paiement} , {self.mission} , {self.montant_total},{self.montant_avance} ,{self.montant_solde}, {self.commission_transitaire},{self.caution_retiree}, {self.caution_remboursee}, {self.date_paiement}, {self.mode_paiement}, {self.observation}"
+
